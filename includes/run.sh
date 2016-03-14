68b0fcbc5ef7410440fba91b0354a48661f2dacb
Leslie-Alexandre DENIS <leslie-alexandre.denis@cc.in2p3.fr>
2016-03-14 15:35:16 +0100

    Use initd service file instead of service cmd

--- includes/run.sh
+++ includes/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-service postgresql start
+/etc/init.d/postgresql start
 sudo -u tomcat7 R CMD Rserve --slave --no-save --no-restore --no-init-file --RS-conf /etc/Rserve.conf
 screen -AdmS SolR make -C /tmp/transmart-data/solr/ start
 sudo -u tomcat7 /usr/share/tomcat7/bin/catalina.sh run
-
 
 # The container will run as long as the script is running, that's why
 # we need something long-lived here
